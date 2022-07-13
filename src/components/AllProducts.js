5261f3342f11ddf4507c5c9f46d6565d69c611c3
MinFTW <kimmins2@outlook.com>
2022-07-13 08:34:37 -0500

    first commit

---
+++ src/components/AllProducts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../style/AllProducts.css';
+
+function AllProducts() {
+  return (
+    <div className='all-products'>
+      <h1>AllProducts</h1>
+    </div>
+  );
+}
+
+export default AllProducts;
---
+++ src/components/SingleProduct.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../style/SingleProduct.css';
+
+function SingleProduct() {
+  return (
+    <div className='single-product'>
+      <h1>Single Product</h1>
+    </div>
+  );
+}
+
+export default SingleProduct;
