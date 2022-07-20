8a94e32f9ec5095973d08aa881d88fa69e5456f1
MinFTW <kimmins2@outlook.com>
2022-07-19 23:44:56 -0500

    completed getAllProducts

--- tests/db/products.spec.js
+++ tests/db/products.spec.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { getAllProducts } = require('../../db/models/products');
+const { Products } = require('../../db/models/');
 const client = require('../../db/client');
 
 let productsFromDatabase, productsFromAdapter;
@@ -12,7 +12,7 @@ describe('Database', () => {
       FROM products;
     `);
     productsFromDatabase = rows;
-    productsFromAdapter = await getAllProducts();
+    productsFromAdapter = await Products.getAllProducts();
   });
   afterAll(async () => {
     client.end();
@@ -26,9 +26,46 @@ describe('Database', () => {
     it('selects and returns an array of products', async () => {
       expect(productsFromAdapter).toEqual(productsFromDatabase);
     });
+    it('Returns greater than zero products', async () => {
+      expect(productsFromAdapter.length).toBeGreaterThan(0);
+    });
+    it('Product is an object', async () => {
+      const [product] = productsFromAdapter;
+      expect(typeof product).toBe('object');
+    });
+    it('each product has a id property', async () => {
+      const [product] = productsFromAdapter;
+      expect(product).toHaveProperty('id');
+    });
     it('each product has a name property', async () => {
       const [product] = productsFromAdapter;
       expect(product).toHaveProperty('name');
+    });
+    it('each product has a description property', async () => {
+      const [product] = productsFromAdapter;
+      expect(product).toHaveProperty('description');
+    });
+    it('each product has a price property', async () => {
+      const [product] = productsFromAdapter;
+      expect(product).toHaveProperty('price');
+    });
+    it('each product has a imageURL property', async () => {
+      const [product] = productsFromAdapter;
+      expect(product).toHaveProperty('imageURL');
+    });
+    it('each product has a inStock property', async () => {
+      const [product] = productsFromAdapter;
+      expect(product).toHaveProperty('inStock');
+    });
+    it('each product has a category property', async () => {
+      const [product] = productsFromAdapter;
+      expect(product).toHaveProperty('category');
+    });
+  });
+
+  describe('getProductById', () => {
+    it('returns an array', async () => {
+      expect(Array.isArray(productsFromAdapter)).toBe(true);
     });
   });
 });
