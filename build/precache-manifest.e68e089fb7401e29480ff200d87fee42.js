6a6635bbafcdddf68d37fbd054dcc295f6265d26
Austin <austin@foodtronix.com>
2022-07-23 11:32:14 -0500

    created orderProducts api and db adapters

---
+++ build/precache-manifest.e68e089fb7401e29480ff200d87fee42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf4aafd69732b4b7ca06f9161208f626",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d01e0151bca2f5dd886",
+    "url": "/static/css/2.5e069dfe.chunk.css"
+  },
+  {
+    "revision": "e6fbae88d28ff6c3e5c1",
+    "url": "/static/css/main.36f920cd.chunk.css"
+  },
+  {
+    "revision": "8d01e0151bca2f5dd886",
+    "url": "/static/js/2.2e0bc42a.chunk.js"
+  },
+  {
+    "revision": "1bde7cbddc37293b60694441b834365b",
+    "url": "/static/js/2.2e0bc42a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6fbae88d28ff6c3e5c1",
+    "url": "/static/js/main.565c31c2.chunk.js"
+  },
+  {
+    "revision": "7e23c3461cd3b1e0abd6",
+    "url": "/static/js/runtime-main.da5551cf.js"
+  }
+]);
