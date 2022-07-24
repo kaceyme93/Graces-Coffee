846610e0a7cba54343fcab7ee41f53855c4e6370
MinFTW <kimmins2@outlook.com>
2022-07-24 17:49:05 -0500

    updated getCartByUser query

--- db/models/orders.js
+++ db/models/orders.js
@@ -93,8 +93,8 @@ async function getCartByUser({ id }) {
       `
        SELECT *
        FROM orders
-       WHERE orders."userId" = $1
-       AND orders.status = "created";
+       WHERE "userId" = $1
+       AND status = 'created';
       `,
       [id]
     );
